e43ef671a0ad21db835f782dfbc1cc757f003ea9
Hakkı YÜCE <1hayuce@gmail.com>
2016-02-29 10:16:34 +0200

    Update wlauthor.py

--- wlauthor.py
+++ wlauthor.py
@@ -25,10 +25,6 @@ if hedef == "1":    # WIFI
                'KIRKLARELİ', 'KIRŞEHİR', 'KOCAELİ', 'KONYA', 'KÜTAHYA', 'MALATYA', 'MANİSA', 'MARAŞ', 'MARDİN', 'MUĞLA', 'MUŞ', 'NEVŞEHİR', 'NİĞDE', 'ORDU', 'RİZE', 'SAKARYA', 'SAMSUN', 'SİİRT',
                'SİNOP', 'SİVAS', 'TEKİRDAĞ', 'TOKAT', 'TRABZON', 'TUNCELİ', 'ŞANLIURFA', 'UŞAK', 'VAN', 'YOZGAT', 'ZONGULDAK', 'AKSARAY', 'BAYBURT', 'KARAMAN', 'KIRIKKALE', 'BATMAN', 'ŞIRNAK', 'BARTIN', 'ARDAHAN',
                'IĞDIR', 'YALOVA', 'KARABÜK', 'OSMANİYE', 'DÜZCE']
-    #plakalar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45',
-    #           '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', ]
-    ### KODU YAZARKEN FARK ETTİK Kİ BU LİSTEYE GEREK YOK ---
-    #                                                                                                            sehirler.index(sehir) in döndürdüğü değer +1  yeterli :P
 
     ssid = raw_input("SSID : ") # Kullanıcıdan SSID için bir değer istiyoruz.
     wifi.append(ssid)                # Başta oluşturduğumuz 'wifi' listesine, kullanıcıdan değer alındıktan sonra ssid değişkenimizi ekliyoruz.
